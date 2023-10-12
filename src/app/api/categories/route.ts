961ec07db881ecacbdfcfab3286a25011314a11f
Medhashis_Maiti <medhashis000@gmail.com>
2023-10-12 19:53:07 +0530

    trial 1 completed : successfully created an api to get all the data that is created in the database

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,16 @@
-import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-export const GET =()=>{
-    return  NextResponse.json({message:"hello"},{status: 200});
-}
+const prisma = new PrismaClient();
+export const GET = async () => {
+  try {
+    // trial 1 -> building an api to fetch all the categories created
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log("there is an error", error);
+    return new NextResponse(JSON.stringify({ message: "error in the get" }), {
+      status: 400,
+    });
+  }
+};
