30bdfb6cfbeb75ec18c4e1017ef815422bc5e82d
Medhashis_Maiti <medhashis000@gmail.com>
2023-11-09 23:09:06 +0530

    fetching all the products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -5,9 +5,9 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    // trial 1 -> building an api to fetch all the categories created
-    const categories = await prisma.category.findMany();
-    return new NextResponse(JSON.stringify(categories), { status: 200 });
+    // trial 1 -> building an api to fetch all the products created
+    const products = await prisma.product.findMany();
+    return new NextResponse(JSON.stringify(products), { status: 200 });
   } catch (error) {
     console.log("there is an error", error);
     return new NextResponse(JSON.stringify({ message: "error in the get" }), {
