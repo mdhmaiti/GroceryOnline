a907730ac95ed632e7bffe73f218ff030345f956
Medhashis_Maiti <medhashis000@gmail.com>
2023-11-23 19:42:26 +0530

    orders for the user and the admin

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -11,8 +11,30 @@ export const GET = async (req: NextRequest) => {
   // once we get the session then if session we do something or otherwise return an error
   if (session) {
     try {
-    } catch (error) {}
+      // try if he is admin show them add the order from all the emails
+      // if he is an user only show their specific orders
+
+      if(session.user.isAdmin){
+        const orders = prisma.order.findMany();
+        return new NextResponse(JSON.stringify(orders),{status:200});
+      }
+      // common user --> user emaul unique
+      const orders = prisma.order.findMany({
+        where:{
+          userEmail:session.user.email!,
+        }
+      })
+      return new NextResponse(JSON.stringify(orders),{status:200});
+
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(JSON.stringify({ message: "not authenticated" }), {
+        status: 500,
+      });
+      
+    }
   } else {
+    
     return new NextResponse(JSON.stringify({ message: "not authenticated" }), {
         status: 500,
       });
