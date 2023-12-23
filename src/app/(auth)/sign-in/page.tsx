96d04ba6eff9f9e0c7de70898ef16f4ef9750a9c
Medhashis_Maiti <medhashis000@gmail.com>
2023-12-24 00:58:00 +0530

    read me update

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>test page</div>
+    <div>test page i will add a separate sign in page later , for now sign in and sign out from the account button </div>
   )
 }
 
