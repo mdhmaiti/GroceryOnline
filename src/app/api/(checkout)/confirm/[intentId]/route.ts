e397ced1e2d4eeeea0b3b520eba4f620a9381688
Medhashis_Maiti <medhashis000@gmail.com>
2024-01-03 19:45:01 +0530

    deploy error fix 1

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,35 +1,35 @@
-import prisma from "@/utils/db";
-import { NextResponse } from "next/server";
+// import prisma from "@/utils/db";
+// import { NextResponse } from "next/server";
 
 
-// to update the order status after the payment 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
-  const { intentId } = params;
+// // to update the order status after the payment 
+// export const PUT = async ({ params }: { params: { intentId: string } }) => {
+//   const { intentId } = params;
 
-  try {
-    const updatedOrder = await prisma.order.update({
-      where: {
-        intent_id: intentId,
-      },
-      data: { status: "Being prepared!" },
-    });
+//   try {
+//     const updatedOrder = await prisma.order.update({
+//       where: {
+//         intent_id: intentId,
+//       },
+//       data: { status: "Being prepared!" },
+//     });
 
-    if (!updatedOrder) {
-      return new NextResponse(JSON.stringify({ message: "Order not found!" }), {
-        status: 404,
-      });
-    }
+//     if (!updatedOrder) {
+//       return new NextResponse(JSON.stringify({ message: "Order not found!" }), {
+//         status: 404,
+//       });
+//     }
 
-    return new NextResponse(
-      JSON.stringify({ message: "Order has been updated" }),
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error updating order:", error);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Order has been updated" }),
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error updating order:", error);
 
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
-      { status: 500 }
-    );
-  }
-};
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }),
+//       { status: 500 }
+//     );
+//   }
+// };
--- src/app/api/(checkout)/createIntent/[orderId]/route.ts
+++ src/app/api/(checkout)/createIntent/[orderId]/route.ts
@@ -1,57 +1,57 @@
-import prisma from "@/utils/db";
-import { NextRequest, NextResponse } from "next/server";
+// import prisma from "@/utils/db";
+// import { NextRequest, NextResponse } from "next/server";
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-// find the order by the order id
-// cases : if there is order then only proceed 
+// // find the order by the order id
+// // cases : if there is order then only proceed 
 
 
-export async function POST(
-  req: NextRequest,
-  { params }: { params: { orderId: string } }
-) {
-    try {
-      const { orderId } = params;
-      const order = await prisma.order.findUnique({
-          where: {
-            id: orderId,
-          },
-        });
+// export async function POST(
+//   req: NextRequest,
+//   { params }: { params: { orderId: string } }
+// ) {
+//     try {
+//       const { orderId } = params;
+//       const order = await prisma.order.findUnique({
+//           where: {
+//             id: orderId,
+//           },
+//         });
   
-        if (order) {
-          const paymentIntent = await stripe.paymentIntents.create({
-            amount: order.price.toNumber()  * 100,
-            currency: "inr",
-            automatic_payment_methods: {
-              enabled: true,
-            },
-            description: `Payment for order ${orderId}`,
-          });
-          await prisma.order.update({
-            where: {
-              id: orderId,
-            },
-            data: { intent_id: paymentIntent.id },
-          });
-          return new NextResponse(
-            JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-            { status: 200 }
-          );
-      }
-      return new NextResponse(
-        JSON.stringify({ message:"Order not found!" }),
-        { status: 404 }
-      );
+//         if (order) {
+//           const paymentIntent = await stripe.paymentIntents.create({
+//             amount: order.price.toNumber()  * 100,
+//             currency: "inr",
+//             automatic_payment_methods: {
+//               enabled: true,
+//             },
+//             description: `Payment for order ${orderId}`,
+//           });
+//           await prisma.order.update({
+//             where: {
+//               id: orderId,
+//             },
+//             data: { intent_id: paymentIntent.id },
+//           });
+//           return new NextResponse(
+//             JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+//             { status: 200 }
+//           );
+//       }
+//       return new NextResponse(
+//         JSON.stringify({ message:"Order not found!" }),
+//         { status: 404 }
+//       );
       
-    } catch (error) {
-      console.error("Error processing payment:", error);
-      return new NextResponse(
-        JSON.stringify({ message: "Error processing payment" }),
-        { status: 500 }
-      );
+//     } catch (error) {
+//       console.error("Error processing payment:", error);
+//       return new NextResponse(
+//         JSON.stringify({ message: "Error processing payment" }),
+//         { status: 500 }
+//       );
       
-    }
+//     }
    
     
-}
+// }
